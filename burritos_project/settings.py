b5a63d4631f4a7ee4807266cf9da02cbf39ff512
AmadorAyalaAngel4-2 <a24311010@uthermosillo.edu.mx>
2025-10-23 20:05:38 -0700

    add alowed host to hosting

--- burritos_project/settings.py
+++ burritos_project/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-k%&ftlqur1)e!sp)n&iim##a45en46$s*usj4kqk)tbljpetj$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "amadorayalabackend.pythonanywhere.com"
+    ]
 
 
 # Application definition
